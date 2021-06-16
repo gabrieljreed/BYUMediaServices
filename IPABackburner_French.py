8fdf3be78c7ef62a1dc74e5acf235b5cfff46ac3
Gabriel Reed <gabrieljreed@gmail.com>
2021-06-16 15:34:58 -0600

    Updated French script to match German

--- IPABackburner_French.py
+++ IPABackburner_French.py
@@ -3,10 +3,47 @@ import maya.mel as mm
 import os
 import getpass
 import tempfile
+import glob
+
+
+# Set general render settings
+def setGeneralRenderSettings():
+    #Duplicate Camera
+    duplicateCam('duplicateCam('Evelyn_FrenchSpeak_Master:Side_Camera')')
+
+    render_default = 'defaultRenderGlobals'
+    render_arnold = 'defaultArnoldDriver'
+    render_glob = render_arnold
+    list_Attr = cmds.listAttr(render_glob, r = False, s = True)
+    for attr in list_Attr:
+        get_attr_name = "%s.%s"%(render_glob, attr)
+        print "stAttr %s %s"%(get_attr_name, cmds.getAttr(get_attr_name))
+
+    #Set file output to tif
+    cmds.setAttr("defaultArnoldDriver.ai_translator", "tif", type="string")
+    cmds.setAttr('defaultArnoldDriver.tiffCompression', 0)
+    cmds.setAttr('defaultArnoldDriver.tiffFormat', 2)
+
+    #Setting the namespace to name_#.ext
+    cmds.setAttr ('defaultRenderGlobals.outFormatControl', 0)
+    cmds.setAttr ('defaultRenderGlobals.animation', 1)
+    cmds.setAttr ('defaultRenderGlobals.putFrameBeforeExt', 1)
+    cmds.setAttr ('defaultRenderGlobals.periodInExt', 2)
+    cmds.setAttr ('defaultRenderGlobals.extensionPadding', 3)
+
+    #Set end frame for rendering
+    anim_end_frame = cmds.playbackOptions(maxTime =True, q=True)
+    cmds.setAttr('defaultRenderGlobals.endFrame', anim_end_frame)
+
+    #Change image size
+    cmds.setAttr('defaultResolution.width',1920)
+    cmds.setAttr('defaultResolution.height',1080)
+    cmds.setAttr('defaultResolution.deviceAspectRatio',1.777)
+
 
 # Generate temp file
 def generateTempFile(jobName, cameraName):
-    print("Generating temp file for ", cmds.file(q=True, sn=True, shn=True))
+    print("Generating temp file for {printFileName}...".format(printFileName=cmds.file(q=True, sn=True, shn=True)))
     taskList = "{jobName}_{cameraName}".format(jobName=jobName, cameraName=cameraName)
     tempFileName = tempfile.gettempdir()
     tempFileName = tempFileName.replace("\\", '/')
@@ -30,11 +67,27 @@ def generateTempFile(jobName, cameraName):
         startTaskFrame += taskSize
         lastTaskFrame += taskSize
     taskListFile.close()
+    print("Temp file successfully generated\n\n")
     return tempFileName
 
 
+# Make folder
+def makeFolder(filePath):
+    slashIndex = filePath.rfind("/")
+    jobName = filePath[slashIndex + 1:].split(".")[0]
+    filePath = filePath[:slashIndex]
+    mode = 0o777
+    path = (os.path.join(filePath, jobName))
+
+    if not os.path.exists(path):
+        os.mkdir(path, mode)
+        print("Created folder at {path}\n".format(path=path))
+    else:
+        print("Folder already exists, skipped creating\n")
+
+
 # Execute Backburner job
-def executeBackburner(cameraName, savePath):
+def executeBackburner(cameraName, savePath, baseFilePath):
     if cameraName is "FrontCam":
         cmds.showHidden('Evelyn_Full')
         cmds.hide('Mouthbag_Full')
@@ -66,30 +119,32 @@ def executeBackburner(cameraName, savePath):
     # Save As a new file
     filePath = cmds.file(q=True, sn=True)
     slashIndex = filePath.rfind("/")
-    # savePath = filePath[:slashIndex]
-    jobName = filePath[slashIndex + 1:].split(".")[0]
-    if jobName.count("_") is not 1: # FIXME: Do it by camera rather than the name since there are going to be a bunch of weird exceptions
+    jobName = filePath[slashIndex + 1:].split(".")[0] # Strips off the path and the .mb
+
+    if cameraName is not "FrontCam": # Strips off extra camera names from file name
         jobName = jobName[:jobName.rfind("_")]
+    print("jobName: {jobName} \n").format(jobName=jobName)
+
     finalFileName = "{jobName}_{cameraName}.mb".format(jobName=jobName, cameraName=cameraName)
     finalSavePath = (os.path.join(savePath, finalFileName)).replace("\\", "/")
-    print("Saving file: ", finalSavePath)
+    print("\n\nSaving file: {finalSavePath}\n".format(finalSavePath=finalSavePath))
     cmds.file(rename=finalSavePath)
     cmds.file(save=True, type="mayaBinary", f=True)
 
-    # Generate temp
+    # Generate temp file
     generateTempFile(jobName, cameraName)
 
     # Generate and execute new Backburner command
     projectPath = r"V:/Animation/2021/BYU Online/French IPA/Projects/French_IPA_Project/Renders/MayaRenders"
-    renderPath = filePath.split("Scenes/")[1]
+    renderPath = baseFilePath.split("Scenes/")[1]
     slashIndex = renderPath.rfind("/")
     renderPath = renderPath[:slashIndex]
     slashIndex = filePath.rfind("/")
     jobFolder = filePath[slashIndex + 1:].split(".")[0]
     finalRenderPath = (os.path.join(projectPath, renderPath, jobName, cameraName)).replace("\\", "/")
-    print("Rendering to : ", finalRenderPath)
-    final = "\"\\\"\\\"C:/Program Files (x86)/Autodesk/Backburner/cmdjob.exe\\\" -jobName \\\"{jobName}_{cameraName}\\\" -description \\\"\\\" -manager 10.25.15.188 -port 7347 -priority 1 -taskList \\\"C:/Users/{username}/AppData/Local/Temp/{jobName}_{cameraName}.txt\\\" -taskName 1 \\\"C:/Program Files/Autodesk/Maya2020/bin/Render\\\" -r file -s %tp2 -e %tp3 -proj \\\"C:/Users/{username}/Documents/maya/projects/default\\\" -rd \\\"{writeDirectory}\\\"  \\\"{projectName}\\\"\"".format(jobName = jobName, username = getpass.getuser(), cameraName = cameraName, projectName = finalSavePath, writeDirectory = finalRenderPath)
-    print("Sending to Backburner")
+    print("Rendering to : {finalRenderPath}".format(finalRenderPath=finalRenderPath))
+    final = "\"\\\"\\\"C:/Program Files (x86)/Autodesk/Backburner/cmdjob.exe\\\" -jobName \\\"{jobName}_{cameraName}\\\" -description \\\"\\\" -manager 10.25.15.188 -port 7347 -priority 25 -taskList \\\"C:/Users/{username}/AppData/Local/Temp/{jobName}_{cameraName}.txt\\\" -taskName 2 \\\"C:/Program Files/Autodesk/Maya2020/bin/Render\\\" -r file -s %tp2 -e %tp3 -proj \\\"C:/Users/{username}/Documents/maya/projects/default\\\" -rd \\\"{writeDirectory}\\\"  \\\"{finalSavePath}\\\"\"".format(jobName = jobName, username = getpass.getuser(), cameraName = cameraName, finalSavePath = finalSavePath, writeDirectory = finalRenderPath)
+    print("\n\nSending to Backburner")
     print(final)
     printSend = mm.eval('system (' + final + ')')
     print printSend
@@ -101,59 +156,30 @@ def duplicateCam(CameraName):
     cmds.duplicate(rr=True, n= 'Evelyn_FrenchSpeak_Master:Mouthbag_Camera')
 
 
-#Duplicate Camera
-duplicateCam('Evelyn_FrenchSpeak_Master:Side_Camera')
+# Main function to send all Backburner jobs for a given file
+def runner(fileName):
+    cmds.file(save=True, type="mayaBinary", f=True)
+    cmds.file(fileName, o=True)
+
+    setGeneralRenderSettings()
+
+    # Create a new folder to put all the extra Maya files into
+    filePath = cmds.file(q=True, sn=True)
+    # makeFolder(filePath)
+
+    path = r"V:/Animation/2021/BYU Online/French IPA/Projects/French_IPA_Project/Maya/Temp_Backburner"
+
+    executeBackburner("FrontCam", path, filePath)
+    executeBackburner("SideCam", path, filePath)
+
+    cmds.setAttr('Evelyn_FrenchSpeak_Master:DomeLightShape.camera', 0)
+    cmds.setAttr('Evelyn_FrenchSpeak_Master:DomeLight.visibility', 0)
+    executeBackburner("MouthBag", path, filePath)
+
+    # Opens original file back up (for debugging purposes)
+    cmds.file(filePath, o=True)
+
+    print("Finished\n\n\n")
 
 
-# MARK: SETS GENERAL RENDER SETTINGS
-render_default = 'defaultRenderGlobals'
-render_arnold = 'defaultArnoldDriver'
-render_glob = render_arnold
-list_Attr = cmds.listAttr(render_glob, r = False, s = True)
-for attr in list_Attr:
-    get_attr_name = "%s.%s"%(render_glob, attr)
-    print "stAttr %s %s"%(get_attr_name, cmds.getAttr(get_attr_name))
-
-# Create a new folder to put all the extra Maya files into
-filePath = cmds.file(q=True, sn=True)
-slashIndex = filePath.rfind("/")
-jobName = filePath[slashIndex + 1:].split(".")[0]
-filePath = filePath[:slashIndex]
-mode = 0o777
-path = (os.path.join(filePath, jobName))
-
-if not os.path.exists(path):
-    os.mkdir(path, mode)
-    print("Created folder at ", path)
-else:
-    print("Folder already exists, skipped creating")
-
-#Set file output to tif
-cmds.setAttr("defaultArnoldDriver.ai_translator", "tif", type="string")
-cmds.setAttr('defaultArnoldDriver.tiffCompression', 0)
-cmds.setAttr('defaultArnoldDriver.tiffFormat', 2)
-
-#Setting the namespace to name_#.ext
-cmds.setAttr ('defaultRenderGlobals.outFormatControl', 0)
-cmds.setAttr ('defaultRenderGlobals.animation', 1)
-cmds.setAttr ('defaultRenderGlobals.putFrameBeforeExt', 1)
-cmds.setAttr ('defaultRenderGlobals.periodInExt', 2)
-cmds.setAttr ('defaultRenderGlobals.extensionPadding', 3)
-
-#Set end frame for rendering
-anim_end_frame = cmds.playbackOptions(maxTime =True, q=True)
-cmds.setAttr('defaultRenderGlobals.endFrame', anim_end_frame)
-
-#Change image size
-cmds.setAttr('defaultResolution.width',1920)
-cmds.setAttr('defaultResolution.height',1080)
-cmds.setAttr('defaultResolution.deviceAspectRatio',1.777)
-
-executeBackburner("FrontCam", path)
-executeBackburner("SideCam", path)
-
-cmds.setAttr('Evelyn_FrenchSpeak_Master:DomeLightShape.camera', 0)
-cmds.setAttr('Evelyn_FrenchSpeak_Master:DomeLight.visibility', 0)
-executeBackburner("MouthBag", path)
-
-print("Finished")
+runner(cmds.file(q=True, sn=True))
